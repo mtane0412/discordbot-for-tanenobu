a1208f174f2de20b1df8e3175ae594a758567d43
mtane0412 <mtane0412@gmail.com>
2023-11-19 22:14:59 +0900

    日本時間を返すように変更

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,8 @@ client.on('messageCreate', async (message: Message) => {
   if (message.author.bot) return
   if (message.content === '!time') {
     const date1 = new Date()
-    void message.channel.send(date1.toLocaleString())
+    // 日本の時刻を送信する
+    void message.channel.send(date1.toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' }))
   }
 })
 
